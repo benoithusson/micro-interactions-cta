7ac4d18d1247937fd9449e52b18fa4638ab5e14c
benoithusson <benoit.husson@liip.ch>
2022-11-06 13:59:29 +0100

    Add new animations

--- js/js-animations.js
+++ js/js-animations.js
@@ -1,35 +1,45 @@
 "use strict";
 
-const bg = document.getElementById("bg");
-const moutainLeft = document.getElementById("moutain_left");
-const moutainRight = document.getElementById("moutain_right");
-const text = document.getElementById("text");
+// const bg = document.getElementById("bg");
+// const moutainLeft = document.getElementById("moutain_left");
+// const moutainRight = document.getElementById("moutain_right");
+// const text = document.getElementById("text");
 
-gsap.to(bg, {
-    scrollTrigger: {
-        scrub: 1,
-    },
-    scale: 1.5
-})
-gsap.from(text, {
-    scrollTrigger: {
-        scrub: 1,
-    },
-    scale: 0.5
-})
+// gsap.to(bg, {
+//     scrollTrigger: {
+//         scrub: 1,
+//     },
+//     scale: 1.5
+// })
+// gsap.from(text, {
+//     scrollTrigger: {
+//         scrub: 1,
+//     },
+//     scale: 0.5
+// })
 
-gsap.to(moutainLeft, {
-    scrollTrigger: {
-        trigger: bg,
-        scrub: 1,
-    },
-    x: '-200px'
-})
+// gsap.to(moutainLeft, {
+//     scrollTrigger: {
+//         trigger: bg,
+//         scrub: 1,
+//     },
+//     x: '-200px'
+// })
 
-gsap.to(moutainRight, {
-    scrollTrigger: {
-        trigger: bg,
-        scrub: 1,
-    },
-    x: '200px'
-})
+// gsap.to(moutainRight, {
+//     scrollTrigger: {
+//         trigger: bg,
+//         scrub: 1,
+//     },
+//     x: '200px'
+// })
+
+
+// let a = 0;
+
+// while (true) {
+//     a = a + 1;
+//     setTimeout(() => {
+//         console.log(a);
+//     }, 1000)
+// }
